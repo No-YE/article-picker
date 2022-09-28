81aa374407af54ce1d4448900dc69c518eb42e80
No-YE <nye7181@gmail.com>
2022-09-28 19:01:04 +0900

    set entity setter convention

--- packages/server/src/domain/model/entity.ts
+++ packages/server/src/domain/model/entity.ts
@@ -1,5 +1,15 @@
 abstract class Entity {
-  protected id: number = -1;
+  protected _id: number = -1;
+
+  protected constructor() {}
+
+  protected set id(value: number) {
+    this._id = value;
+  }
+
+  static new(_args: unknown) {
+    throw Error();
+  }
 }
 
 export { Entity };
--- packages/server/src/domain/model/user/entity.ts
+++ packages/server/src/domain/model/user/entity.ts
@@ -1,13 +1,13 @@
 import { Entity } from '../entity';
 
 class User extends Entity {
-  private name!: string;
-  private email!: string;
-  private createdAt!: Date;
-  private updatedAt!: Date;
-  private deletedAt?: Date | null;
+  private _name!: string;
+  private _email!: string;
+  private _createdAt!: Date;
+  private _updatedAt!: Date;
+  private _deletedAt?: Date | null;
 
-  static new(param: {
+  static new({ id, name, email, createdAt, updatedAt }: {
     id?: number,
     name: string,
     email: string,
@@ -15,20 +15,46 @@ class User extends Entity {
     updatedAt?: Date,
   }): User {
     const entity = new this();
-    Object.assign(entity, { id: -1, createdAt: -1, updatedAt: -1 }, param);
+    entity.id = id ?? -1;
+    entity.name = name;
+    entity.email = email;
+    entity.createdAt = createdAt ?? new Date('');
+    entity.updatedAt = updatedAt ?? new Date('');
 
     return entity;
   }
 
   get properties() {
     return {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt,
-      deletedAt: this.deletedAt,
+      id: this._id,
+      name: this._name,
+      email: this._email,
+      createdAt: this._createdAt,
+      updatedAt: this._updatedAt,
+      deletedAt: this._deletedAt,
     };
+  }
+
+  private set name(value: string) {
+    if (value.length < 1) throw Error();
+
+    this._name = value;
+  }
+
+  private set email(value: string) {
+    if (value.length < 1) throw Error();
+
+    this._email = value;
+  }
+
+  private set createdAt(value: Date) {
+    this._createdAt = value;
+  }
+
+  private set updatedAt(value: Date) {
+    if (value < this.createdAt) throw Error();
+
+    this._updatedAt = value;
   }
 }
 
