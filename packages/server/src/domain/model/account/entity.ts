3750a9cfddead1de334633632c23cf6d13af2a4e
No-YE <nye7181@gmail.com>
2022-10-07 22:59:45 +0900

    remove useless field in account entity

--- packages/server/src/domain/model/account/entity.ts
+++ packages/server/src/domain/model/account/entity.ts
@@ -3,11 +3,8 @@ import { Entity } from '../entity';
 class Account extends Entity {
   name!: string;
   email!: string;
-  private createdAt!: Date;
-  private updatedAt!: Date;
-  private deletedAt?: Date | null;
 
-  static new({ id, name, email, createdAt, updatedAt }: {
+  static new({ id, name, email }: {
     id?: number,
     name: string,
     email: string,
@@ -18,8 +15,6 @@ class Account extends Entity {
     entity.setId(id ?? -1);
     entity.setName(name);
     entity.setEmail(email);
-    entity.setCreatedAt(createdAt ?? new Date(''));
-    entity.setUpdatedAt(updatedAt ?? new Date(''));
 
     return entity;
   }
@@ -34,16 +29,6 @@ class Account extends Entity {
     if (value.length < 1) throw Error();
 
     this.email = value;
-  }
-
-  private setCreatedAt(value: Date) {
-    this.createdAt = value;
-  }
-
-  private setUpdatedAt(value: Date) {
-    if (value < this.createdAt) throw Error();
-
-    this.updatedAt = value;
   }
 }
 
